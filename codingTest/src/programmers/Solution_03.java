4407d567067a0c70655fd4ce867c453ccf5de445
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-04-05 17:57:13 +0900

    1 - k번째수

---
+++ codingTest/src/programmers/Solution_03.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Solution_03 {
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i = 0; i < commands.length; i++) {
+        	
+        	int[] list = new int[commands[i][1] - commands[i][0] + 1];
+        	
+        	for(int j = 0; j < list.length; j++) {
+        		list[j] = array[j + commands[i][0] - 1];
+        	}
+        	Arrays.sort(list);
+        	answer[i] = list[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+
+}
