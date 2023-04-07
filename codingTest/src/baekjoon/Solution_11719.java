fde8f7b9de0747e9f23a677ae610c3784b240178
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-04-07 18:16:46 +0900

    백준 - 그대로 출력하기 2

---
+++ codingTest/src/baekjoon/Solution_11719.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Solution_11719 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextLine()) {
+			String s = sc.nextLine();
+			System.out.println(s);
+		}
+		sc.close();
+	}
+
+}
