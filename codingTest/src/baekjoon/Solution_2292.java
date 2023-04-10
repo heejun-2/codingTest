0be5b421c751b6a6ee1f9a9aea417749c4badda0
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-04-10 13:30:43 +0900

    백준 - 벌집

---
+++ codingTest/src/baekjoon/Solution_2292.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Solution_2292 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int count = 1;
+		int range = 2;
+		
+		if(N == 1) {
+			System.out.println(1);
+		}else {
+			while(range <= N) {
+				range = range + (6 * count);
+				count++;
+			}
+			System.out.println(count);
+		}
+	}
+
+}
