05a58478688f2eff034eed1e71f8c49918da9392
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-04-10 16:24:17 +0900

    백준 - 수 정렬하기 3

---
+++ codingTest/src/baekjoon/Solution_10989.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution_10989 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < N; i++) {
+			sb.append(arr[i]).append('\n');
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
