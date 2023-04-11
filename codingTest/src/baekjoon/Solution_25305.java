847707dc2e32eec95896028c0dde57436fc8659d
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-04-11 15:21:55 +0900

    백준 - 커트라인

---
+++ codingTest/src/baekjoon/Solution_25305.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution_25305 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int k = sc.nextInt();
+		
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[N-k]);
+	}
+
+}
