9f67dbd548cd5362863eece101614ef73b3cb04c
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-04-17 13:33:21 +0900

    백준 - 재귀의 귀재

--- codingTest/src/baekjoon/Solution_25501.java
+++ codingTest/src/baekjoon/Solution_25501.java
@@ -6,17 +6,6 @@ import java.io.InputStreamReader;
 
 public class Solution_25501 {
 
-    public static int recursion(String s, int l, int r){
-    	count++;
-        if(l >= r) return 1;
-        else if(s.charAt(l) != s.charAt(r)) return 0;
-        else return recursion(s, l+1, r-1);
-    }
-    public static int isPalindrome(String s){
-    	int count = 0;
-        return recursion(s, 0, s.length()-1);
-    }
-    
     static int count = 0;
     
     public static void main(String[] args) throws IOException{
@@ -33,6 +22,18 @@ public class Solution_25501 {
     	br.close();
     	System.out.println(sb.toString());
 
+    }
+    
+    public static int recursion(String s, int l, int r){
+    	count++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    
+    public static int isPalindrome(String s){
+    	int count = 0;
+        return recursion(s, 0, s.length()-1);
     }
 
 }
