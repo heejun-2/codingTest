ba7da9a5a0ac9701c0a591dd7da0a3465437cf4e
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-04-18 15:39:15 +0900

    백준 - 수 정렬하기 2

---
+++ codingTest/src/baekjoon/Solution_2751.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution_2751 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(list);
+		
+		for(int val : list) {
+			sb.append(val).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+
+}
