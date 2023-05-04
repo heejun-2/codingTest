492736ce46466b8fb3d36be6755a4402b4d38db7
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-05-04 14:52:08 +0900

    백준 - ATM

---
+++ codingTest/src/baekjoon/Solution_11399.java
@@ -0,0 +1,39 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_11399 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int time = 0;
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j <= i; j++) {
+				time += arr[j];
+			}
+		}
+		
+		System.out.println(time);
+
+	}
+
+}
