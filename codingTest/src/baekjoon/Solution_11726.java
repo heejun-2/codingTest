761b0438ac53e6c9c88f97afccc362b6b4ca7c04
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-05-06 14:37:15 +0900

    백준 - 2xn 타일링

---
+++ codingTest/src/baekjoon/Solution_11726.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_11726 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[1001];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for(int i = 3; i <= N; i++) {
+			dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+		}
+		
+		System.out.println(dp[N]);
+		
+	}
+
+}
