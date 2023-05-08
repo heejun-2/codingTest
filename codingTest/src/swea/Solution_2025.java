c715ad5fb3fdceb25172ebfb8b4817301035df46
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-05-08 16:34:47 +0900

    swea - N줄 덧셈

---
+++ codingTest/src/swea/Solution_2025.java
@@ -0,0 +1,25 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_2025 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int sum = 0;
+		
+		while(N > 0) {
+			sum += N;
+			N--;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
