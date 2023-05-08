00f0e14e1e4d845f535021e9d5bd446dae9eb45c
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-05-08 13:43:44 +0900

    swea - 신문 헤드라인

---
+++ codingTest/src/swea/Solution_2047.java
@@ -0,0 +1,19 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_2047 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		System.out.println(s.toUpperCase());
+
+	}
+
+}
