bad30d898531a4b51b9d033d3e7125821e73ff86
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-05-08 19:04:10 +0900

    swea - 아주 간단한 계산기

---
+++ codingTest/src/swea/Solution_1938.java
@@ -0,0 +1,26 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_1938 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+
+	}
+
+}
