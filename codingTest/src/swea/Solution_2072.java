342f17571fe3f171eab1981c436c696aca9d2bcf
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-05-08 13:08:59 +0900

    swea - 홀수만 더하기

---
+++ codingTest/src/swea/Solution_2072.java
@@ -0,0 +1,36 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_2072 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		for(int i = 1; i <= T; i++) {
+			int sum = 0;
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			while(st.hasMoreTokens()) {
+				int tmp = Integer.parseInt(st.nextToken());
+				
+				if(tmp % 2 == 1) {
+					sum += tmp;
+				}
+				
+			}
+			
+			System.out.println("#"+i+" "+sum);
+		}
+
+	}
+
+}
