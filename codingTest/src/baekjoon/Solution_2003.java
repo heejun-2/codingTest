a2800c399ea6072c20bb71a9fa2d542edcee8ba7
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-05-12 16:44:31 +0900

    백준 - 수들의 합 2

---
+++ codingTest/src/baekjoon/Solution_2003.java
@@ -0,0 +1,50 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_2003 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int start = 0;
+		int end = 0;
+		int sum = 0;
+		int cnt = 0;
+		
+		while(start < N) {
+			if(sum > M || end == N) {
+				sum -= arr[start];
+				start++;
+			}else {
+				sum += arr[end];
+				end++;
+			}
+			
+			if(sum == M) {
+				cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
+
+	}
+
+}
