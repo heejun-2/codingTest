4c4355ae78d8d303b5d67ad7555dc41b853acc51
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-06-16 13:03:51 +0900

    백준 - 분수찾기

---
+++ codingTest/src/baekjoon/Solution_1193.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_1193 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int X = Integer.parseInt(br.readLine());
+		
+		int cross_count = 1;
+		int prev_count_sum = 0;
+		
+		while(true) {
+			if (X <= prev_count_sum + cross_count) {	
+				
+				if (cross_count % 2 == 1) {	// 대각선의 개수가 홀수라면 
+					// 분자가 큰 수부터 시작
+					// 분자는 대각선상 블럭의 개수 - (X 번째 - 직전 대각선까지의 블럭 개수 - 1) 
+					// 분모는 X 번째 - 직전 대각선까지의 블럭 개수
+					System.out.print((cross_count - (X - prev_count_sum - 1)) + "/" + (X - prev_count_sum));
+					break;
+				} 
+				
+				else {	// 대각선의 개수가 짝수라면 
+					// 홀수일 때의 출력을 반대로 
+					System.out.print((X - prev_count_sum) + "/" + (cross_count - (X - prev_count_sum - 1)));
+					break;
+				}
+ 
+			} else {
+				prev_count_sum += cross_count;
+				cross_count++;
+			}
+		}
+
+	}
+
+}
