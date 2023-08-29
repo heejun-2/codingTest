f9dcd5ed7f43cc0eb5699194c5301ea34806cace
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-08-29 16:33:37 +0900

    백준 - 수들의 합 5

---
+++ codingTest/src/baekjoon/Solution_2018.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_2018 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		
+		for(int i = 1; i <= N; i++) {
+			int sum = 0;
+			for(int j = i; j <= N; j++) {
+				sum += j;
+				if(sum > N) break;
+				else if(sum == N) {
+					cnt++;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+
+}
