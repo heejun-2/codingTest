dece47060c2e017992ee8cde16c89af409414d69
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-09-01 15:29:18 +0900

    백준 - 스네이크버드

---
+++ codingTest/src/baekjoon/Solution_16435.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_16435 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < N; i++) {
+			if(L >= arr[i]) {
+				L++;
+			}
+			else {
+				break;
+			}
+		}
+		
+		System.out.println(L);
+
+	}
+
+}
