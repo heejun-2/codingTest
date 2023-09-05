2a655ba733645763c83260383aa0398254fb071a
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-09-05 16:56:17 +0900

    프로그래머스 - 가장 가까운 같은 글자

---
+++ codingTest/src/programmers/Solution_05.java
@@ -0,0 +1,30 @@
+package programmers;
+
+public class Solution_05 {
+	// 가장 가까운 같은 글자
+	 public int[] solution(String s) {
+	        int[] answer = new int[s.length()];
+	        
+	        answer[0] = -1;
+	        
+	        for(int i = 1; i < s.length(); i++){
+	            char c = s.charAt(i);
+	            boolean isPass = false;
+	            
+	            for(int j = i - 1; j >= 0; j--){
+	                char target = s.charAt(j);
+	                if(c == target) {
+	                    answer[i] = i - j;
+	                    isPass = true;
+	                    break;
+	                }
+	            }
+	            
+	            if(!isPass) answer[i] = -1;
+	            
+	        }
+	        
+	        return answer;
+	    }
+
+}
