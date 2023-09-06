73a6de0d9dbf45d17a96d084c218aea768052840
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-09-06 19:27:06 +0900

    프로그래머스 - 과일 장수

---
+++ codingTest/src/programmers/Solution_13.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Solution_13 {
+	// 과일 장수
+	public int solution(int k, int m, int[] score) {
+        int answer = 0;
+
+        Arrays.sort(score);
+        
+        for(int i = score.length - m; i >= 0; i -= m){
+            answer += score[i] * m;
+        }
+        
+        return answer;
+    }
+
+}
