63b9fe590f8305d6134e5eaf82afb7b1b4479b93
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-09-07 17:23:21 +0900

    프로그래머스 = 3진법 뒤집기

---
+++ codingTest/src/programmers/Solution_16.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Solution_16 {
+	// 3진법 뒤집기
+	public int solution(int n) {
+        String str = "";
+        
+        while(n != 0){
+            str += n % 3;
+            n /= 3;
+        }
+        
+        return Integer.parseInt(str, 3);
+    }
+
+}
