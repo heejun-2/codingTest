cedb344ee0203b22cdf18e174d42f0cb41b24e09
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-09-11 19:02:42 +0900

    프로그래머스 - 나머지가 1이 되는 수 찾기

---
+++ codingTest/src/programmers/Solution_27.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class Solution_27 {
+	// 나머지가 1이 되는 수 찾기
+	public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 1; i < n; i++){
+            if(n % i == 1){
+                answer = i;
+                break;
+            }
+        }
+        
+        return answer;
+        
+    }
+
+}
