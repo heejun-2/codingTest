6577132b73a409e614a9e5c465dc5e9a0f9812b7
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-09-11 19:34:09 +0900

    프로그래머스 - 평균 구하기

---
+++ codingTest/src/programmers/Solution_28.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class Solution_28 {
+	// 평균 구하기
+	public double solution(int[] arr) {
+        
+        double answer = 0;
+        double sum = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        
+        answer = sum / arr.length;
+        
+        return answer;
+    }
+
+}
