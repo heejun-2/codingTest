80274c788d6a557e0005ca567bb998316c7953c5
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-09-14 23:32:34 +0900

    프로그래머스 - 두 정수 사이의 합

---
+++ codingTest/src/programmers/Solution_37.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class Solution_37 {
+	// 두 정수 사이의 합
+	public long solution(int a, int b) {
+        long answer = 0;
+             
+        int max = Math.max(a,b);
+        int min = Math.min(a,b);
+        
+        for(int i = min; i <= max; i++){
+            answer += i;
+        }
+        
+        return answer;
+    }
+
+}
