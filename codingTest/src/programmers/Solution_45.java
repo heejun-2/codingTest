3bc3f415817158d20d1d3bc9fcf873102e3716f5
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-09-17 21:36:39 +0900

    프로그래머스 - 가운데 글자 가져오기

---
+++ codingTest/src/programmers/Solution_45.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Solution_45 {
+	// 가운데 글자 가져오기
+	public String solution(String s) {
+        String answer = "";
+        
+        int length = s.length();
+        
+        if(length % 2 == 1){
+            answer += s.substring(length / 2, length / 2 + 1);
+        }
+        
+        else{
+            answer += s.substring(length / 2 - 1, length / 2 + 1);
+        }
+        
+        return answer;
+    }
+
+}
