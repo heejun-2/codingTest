2dd90251a58bf1377532b41e00e944d8d329ff85
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-09-17 22:14:48 +0900

    프로그래머스 - 수박수박수박수박수박수?

---
+++ codingTest/src/programmers/Solution_46.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class Solution_46 {
+	// 수박수박수박수박수박수?
+	public String solution(int n) {
+        String answer = "";
+        
+        for(int i = 0; i < n; i++){
+            answer += (i % 2 == 0) ? "수" : "박";
+        }
+        return answer;
+    }
+
+}
