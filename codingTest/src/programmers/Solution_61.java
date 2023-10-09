c695224177aa89f23baf445cbbe6fd3624c2ad17
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-10-09 14:12:22 +0900

    프로그래머스 - 최소직사각형

---
+++ codingTest/src/programmers/Solution_61.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class Solution_61 {
+	// 최소직사각형
+	public int solution(int[][] sizes) {
+        int row = 0;
+        int col = 0;
+        
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][0] > sizes[i][1]){
+                int tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+            row = Math.max(row,sizes[i][0]);
+            col = Math.max(col,sizes[i][1]);
+        }
+        
+        return row * col;
+    }
+
+}
