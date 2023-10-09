bbea8d414cddde5f39aed461c3d6d59ed2adfce4
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-10-09 14:21:20 +0900

    프로그래머스 - 숫자 문자열과 영단어

---
+++ codingTest/src/programmers/Solution_63.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Solution_63 {
+	// 숫자 문자열과 영단어
+	public int solution(String s) {
+    	String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String[] word = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for(int i = 0; i < 10; i++) {
+        	s = s.replace(word[i], num[i]);
+        }
+        
+        return Integer.parseInt(s);
+    }
+
+}
