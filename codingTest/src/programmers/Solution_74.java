173f94cd59c17b2410048ef6f454ecf964b8e21a
ghktk <ghktkf012@naver.com>
2023-10-15 20:00:12 +0900

    프로그래머스 - 폰켓몬

---
+++ codingTest/src/programmers/Solution_74.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.HashSet;
+
+public class Solution_74 {
+    // 폰켓몬
+    public int solution(int[] nums) {
+        int answer = 0;
+        int maxPick = nums.length / 2;
+
+        HashSet<Integer> hs = new HashSet<>();
+
+        for(int i : nums){
+            hs.add(i);
+        }
+
+        if(maxPick > hs.size()){
+            answer = hs.size();
+        }else{
+            answer = maxPick;
+        }
+
+        return answer;
+    }
+}
