17461c22732582b97701a0bd62bff1c1c2b3f9b7
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-10-31 16:46:29 +0900

    프로그래머스 - 체육복

---
+++ codingTest/src/programmers/Solution_117.java
@@ -0,0 +1,36 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Solution_117 {
+	// 체육복
+	public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+        
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        for(int i = 0; i < lost.length; i++) {
+        	for(int j = 0; j < reserve.length; j++) {
+        		if(lost[i] == reserve[j]) {
+        			answer++;
+        			lost[i] = reserve[j] = -1;
+        			break;
+        		}
+        	}
+        }
+        
+        for(int i = 0; i < lost.length; i++) {
+        	for(int j = 0; j < reserve.length; j++) {
+        		if(reserve[j] == lost[i] - 1 || reserve[j] == lost[i] + 1) {
+        			answer++;
+        			reserve[j] = -1;
+        			break;
+        		}
+        	}
+        }
+        
+        return answer;
+    }
+
+}
