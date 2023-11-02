4c00b1f26fa4b16f157b1199e7902f8e3fc5d432
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-11-02 09:48:16 +0900

    프로그래머스 - 공배수

---
+++ codingTest/src/programmers/Solution_125.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class Solution_125 {
+	// 공배수
+	public int solution(int number, int n, int m) {
+        int answer = 0;
+        
+        if(number % n == 0 && number % m == 0) {
+        	return 1;
+        }
+        else {
+        	return answer;
+        }
+        
+    }
+
+}
