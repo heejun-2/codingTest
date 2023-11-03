9a6f4d3427530ea24b788a3a3b193042c688936b
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-11-03 09:50:48 +0900

    프로그래머스 - 숫자 비교하기

---
+++ codingTest/src/programmers/Solution_129.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class Solution_129 {
+	// 숫자 비교하기
+	public int solution(int num1, int num2) {
+        if(num1 == num2) {
+        	return 1;
+        }else {
+        	return -1;
+        }
+    }
+
+}
