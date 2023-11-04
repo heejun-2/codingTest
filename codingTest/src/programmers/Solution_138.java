f865090e817a85658889c6b9434252b8b7efec9f
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-11-04 13:55:10 +0900

    프로그래머스 - 공백으로 구분하기 1

---
+++ codingTest/src/programmers/Solution_138.java
@@ -0,0 +1,10 @@
+package programmers;
+
+public class Solution_138 {
+	// 공백으로 구분하기 1
+	public String[] solution(String my_string) {
+        String[] answer = my_string.split(" ");
+        return answer;
+    }
+
+}
