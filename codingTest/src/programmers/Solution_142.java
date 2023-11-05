c17197b5f58712eada86f75c755ee116db3773b1
ghktk <ghktkf012@naver.com>
2023-11-05 22:05:54 +0900

    프로그래머스 - 0 떼기

---
+++ codingTest/src/programmers/Solution_142.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class Solution_142 {
+    // 0 떼기
+    public String solution(String n_str) {
+        int num = Integer.valueOf(n_str);
+        String answer = String.valueOf(num);
+
+        return answer;
+    }
+}
