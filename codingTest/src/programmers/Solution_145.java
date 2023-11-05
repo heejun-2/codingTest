e9865e7c5d2321c90a2e123ffdd16bfde45333d6
ghktk <ghktkf012@naver.com>
2023-11-06 08:56:44 +0900

    프로그래머스 - 두 수의 합

---
+++ codingTest/src/programmers/Solution_145.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class Solution_145 {
+    // 두 수의 합
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
