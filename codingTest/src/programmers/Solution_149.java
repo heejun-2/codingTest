94cb42d0f65fc2d620cb28b835867d6a7fa30745
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-11-07 10:48:02 +0900

    프로그래머스 - 카운트 업

---
+++ codingTest/src/programmers/Solution_149.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Solution_149 {
+	// 카운트 업
+	public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+        int idx = 0;
+        
+        for(int i = start_num; i <= end_num; i++) {
+        	answer[idx++] = i;
+        }
+        
+        return answer;
+    }
+
+}
