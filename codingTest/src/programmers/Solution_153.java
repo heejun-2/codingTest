9719e7fbb54451bd19766f4348011d0fb89cd35e
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-11-08 12:57:55 +0900

    프로그래머스 - 두 수의 나눗셈

---
+++ codingTest/src/programmers/Solution_153.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class Solution_153 {
+	// 두 수의 나눗셈
+	public int solution(int num1, int num2) {
+        int answer = (int)((double)num1 / (double)num2 * 1000);
+        
+        return answer;
+    }
+
+}
