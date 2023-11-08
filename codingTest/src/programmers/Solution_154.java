717a9b7ca5d03701a44c2135c23f20f40cb87295
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-11-08 12:59:08 +0900

    프로그래머스 - 소문자로 바꾸기

---
+++ codingTest/src/programmers/Solution_154.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class Solution_154 {
+	// 소문자로 바꾸기
+	public String solution(String myString) {
+        String answer = myString.toLowerCase();
+        
+        return answer;
+    }
+
+}
