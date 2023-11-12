4e303d4418a5104ee979075d68ea1dc4a06cf59f
ghktk <ghktkf012@naver.com>
2023-11-12 17:31:45 +0900

    프로그래머스 - 문자열을 정수로 변환하기

---
+++ codingTest/src/programmers/Solution_170.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class Solution_170 {
+    // 문자열을 정수로 변환하기
+    public int solution(String n_str) {
+        int answer = Integer.valueOf(n_str);
+        return answer;
+    }
+}
