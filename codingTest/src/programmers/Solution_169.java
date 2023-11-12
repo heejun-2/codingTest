3682af19f482369066a01d135e2232589860153c
ghktk <ghktkf012@naver.com>
2023-11-12 17:31:19 +0900

    프로그래머스 - 대문자로 바꾸기

---
+++ codingTest/src/programmers/Solution_169.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class Solution_169 {
+    // 대문자로 바꾸기
+    public String solution(String myString) {
+        String answer = myString.toUpperCase();
+        return answer;
+    }
+}
