d55e87de967e9ea915ae3c126b23aebec39c04d0
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-11-14 16:54:38 +0900

    swea - 상원이의 연속 합

--- codingTest/src/swea/Solution_7510.java
+++ codingTest/src/swea/Solution_7510.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution_7510 {
-
+	// 상원이의 연속 합
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,10 +19,13 @@ public class Solution_7510 {
 			
 			int sum = 0;
 			int cnt = 0;
+			if(N != 1) {
+				cnt = 1;
+			}
 			
-			for(int i = 1; i <= N; i++) {
+			for(int i = 1; i <= N / 2 + 1; i++) {
 				sum = 0;
-				for(int j = i; j <= N; j++) {
+				for(int j = i; j <= N / 2 + 1; j++) {
 					sum += j;
 					if(sum == N) cnt++;
 					if(sum >= N) break;
