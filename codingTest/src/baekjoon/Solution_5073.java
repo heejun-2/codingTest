a0e1333e2a13871aa19ff7a5f2050bc860990d0c
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-11-21 10:50:46 +0900

    백준 - 삼각형과 세 변

---
+++ codingTest/src/baekjoon/Solution_5073.java
@@ -0,0 +1,49 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_5073 {
+	// 삼각형과 세 변
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int[] arr = new int[3];
+		
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			arr[0] = Integer.parseInt(st.nextToken());
+			arr[1] = Integer.parseInt(st.nextToken());
+			arr[2] = Integer.parseInt(st.nextToken());
+			
+			if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
+			
+			Arrays.sort(arr);
+			
+			if(arr[2] >= arr[0] + arr[1]) {
+				sb.append("Invalid");
+			}
+			else if(arr[0] == arr[1] && arr[1] == arr[2]) {
+				sb.append("Equilateral");
+			}
+			else if(arr[0] != arr[1] && arr[1] != arr[2] && arr[0] != arr[2]) {
+				sb.append("Scalene");
+			}
+			else {
+				sb.append("Isosceles");
+			}
+			
+			sb.append("\n");
+			
+			
+		}
+		
+		System.out.println(sb.toString());
+		
+
+	}
+
+}
