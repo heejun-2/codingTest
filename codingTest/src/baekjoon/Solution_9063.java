9e51fe43201e3c8f43975a4d7808cf73fff254bd
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-11-21 10:37:36 +0900

    백준 - 대지

---
+++ codingTest/src/baekjoon/Solution_9063.java
@@ -0,0 +1,45 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_9063 {
+	// 대지
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int maxX = Integer.MIN_VALUE;
+		int minX = Integer.MAX_VALUE;
+		int maxY = Integer.MIN_VALUE;
+		int minY = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			if(x > maxX) maxX = x;
+			if(x < minX) minX = x;
+			if(y > maxY) maxY = y;
+			if(y < minY) minY = y;
+			
+			
+		}
+		
+		int width = maxX - minX;
+		int length = maxY - minY;
+		
+		sb.append(width * length).append("\n");
+		
+		System.out.println(sb.toString());
+
+	}
+
+
+}
