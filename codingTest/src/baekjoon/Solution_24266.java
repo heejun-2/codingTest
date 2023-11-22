891764fca01ca4f987cdd026e93f73a88db403df
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2023-11-22 09:23:03 +0900

    백준 - 알고리즘 수업 : 알고리즘의 수행 시간 5

---
+++ codingTest/src/baekjoon/Solution_24266.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_24266 {
+	// 알고리즘 수업 - 알고리즘의 수행 시간 5
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		long n = Long.parseLong(br.readLine());
+		
+		System.out.println(n*n*n);
+		System.out.println(3);
+
+	}
+
+}
